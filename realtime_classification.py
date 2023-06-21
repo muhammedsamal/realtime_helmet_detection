d8090737ed47c9c22e926dfcaae20b5461277600
rishinpoolat <91110305+rishinpoolat@users.noreply.github.com>
2023-06-21 19:06:44 +0530

    updatede

--- realtime_classification.py
+++ realtime_classification.py
@@ -2,6 +2,15 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import os
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.setProperty('rate', 150)
+
+engine.setProperty('volume', 0.8)
+
+text = 'please wear helmet'
 
 # Define the function to retrieve class labels
 def get_class_label_from_index(index):
@@ -30,20 +39,22 @@ while True:
 
     # Pass the image through the model
     prediction = model.predict(img)
-
+    # [[0.325, 0.675]]
     # Analyze the output
     class_idx = np.argmax(prediction[0])
     class_label = get_class_label_from_index(class_idx)
     if class_label == 'no_helmet':
-        os.system("say 'Please wear helmet'")
+        engine.say(text)
+        engine.runAndWait()
+        # os.system("say 'Please wear helmet'")
     confidence = prediction[0][class_idx]
 
     # Draw the predicted class label and confidence on the image
-    text = f"{class_label} {confidence:.2f}"
-    cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    # text = f"{class_label} {confidence:.2f}"
+    # cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     # Show the image with the predicted label
-    cv2.imshow('Real-time Image Classification', frame)
+    # cv2.imshow('Real-time Image Classification', frame)
 
     # Exit the program when the 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
